df3c1683178c74543d98a536c31032d272635b47
Emir Šahović <emir.sahovic00@gmail.com>
2022-09-29 13:58:18 +0200

    Added logo to the custom drawer navigator

--- navigator/CustomDrawerContent.jsx
+++ navigator/CustomDrawerContent.jsx
@@ -1,19 +1,22 @@
-import { Button, View, TouchableOpacity, StyleSheet, Text } from "react-native";
+import { View, TouchableOpacity, StyleSheet, Text, Image } from "react-native";
 import { FontAwesome, AntDesign } from "@expo/vector-icons";
 
 function CustomDrawerContent({ navigation }) {
   return (
     <View style={{ flex: 1, backgroundColor: "#222" }}>
-      <TouchableOpacity onPress={() => navigation.closeDrawer()}>
-        <FontAwesome style={{ alignSelf: "flex-end", marginRight: 12, marginTop: 15 }} name="close" size={25} color="#ddd" />
-      </TouchableOpacity>
-      <View style={[styles.optionsContainer, { marginTop: 35 }]}>
+      <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center", paddingHorizontal: 15, paddingTop: 10 }}>
+        <Image source={require("../assets/img/logo.png")} style={{ width: 40, height: 40 }} />
+        <TouchableOpacity onPress={() => navigation.closeDrawer()}>
+          <FontAwesome name="close" size={25} color="#ddd" />
+        </TouchableOpacity>
+      </View>
+      <View style={[styles.optionsContainer, { marginTop: 45 }]}>
         <FontAwesome name="home" size={30} color="#ddd" />
         <TouchableOpacity onPress={() => navigation.navigate("Home")}>
           <Text style={styles.menuText}>Home</Text>
         </TouchableOpacity>
       </View>
-      <View style={[styles.optionsContainer, { marginTop: 15 }]}>
+      <View style={[styles.optionsContainer, { marginTop: 20 }]}>
         <AntDesign name="pluscircle" size={26} color="#ddd" />
         <TouchableOpacity onPress={() => navigation.navigate("AddPizza")}>
           <Text style={styles.menuText}>Add Pizza</Text>
