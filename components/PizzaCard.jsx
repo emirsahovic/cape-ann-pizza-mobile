b2c7cffcbb7e5a876482fd99178919d388a9529e
Emir Šahović <emir.sahovic00@gmail.com>
2022-09-29 14:01:57 +0200

    Delete pizza functionality

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -1,14 +1,45 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity, Pressable } from "react-native";
-import { FontAwesome, Feather } from "@expo/vector-icons";
+import { StyleSheet, Text, View, Image, TouchableOpacity, Pressable, ActivityIndicator } from "react-native";
+import { FontAwesome, Feather, AntDesign } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
+
+import { ACTION_TYPES } from "../pizza/pizzaActionTypes";
+import { INITIAL_STATE, pizzaReducer } from "../pizza/pizzaReducer";
+import { useReducer } from "react";
+import apiService from "../service/apiService";
 
 const PizzaCard = ({ item, index, lastIndex }) => {
   const navigation = useNavigation();
+
+  const [state, dispatch] = useReducer(pizzaReducer, INITIAL_STATE);
+
+  const handleDelete = async (id) => {
+    try {
+      dispatch({ type: ACTION_TYPES.DELETE_PIZZA_REQUEST });
+      const res = await apiService.delete(`/pizza/${id}`);
+      dispatch({ type: ACTION_TYPES.DELETE_PIZZA_SUCCESS, payload: res.data.id });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  if (state.loading) {
+    return (
+      <View style={styles.loading}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
 
   return (
     <TouchableOpacity onPress={() => navigation.navigate("PizzaDetail", { id: item.id })}>
       <View style={[styles.pizzaCard, index === lastIndex ? { marginRight: 0 } : { marginRight: 20 }]}>
         <Image style={styles.image} source={{ uri: item.picture }} />
+        <AntDesign
+          name="closecircle"
+          size={20}
+          style={{ color: "red", position: "absolute", top: 5, right: 5 }}
+          onPress={() => handleDelete(item.id)}
+        />
         <Text style={styles.pizzaName}>{item.name}</Text>
         <View style={styles.priceContainer}>
           <View style={{ flexDirection: "row", alignItems: "center" }}>
@@ -43,11 +74,12 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     padding: 10,
     width: 210,
+    position: "relative",
   },
   image: {
     width: "100%",
     height: 120,
-    resizeMode: "contain",
+    resizeMode: "cover",
     borderRadius: 10,
   },
   pizzaName: {
@@ -66,5 +98,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-between",
     marginTop: 10,
+  },
+  loading: {
+    backgroundColor: "#272629",
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
