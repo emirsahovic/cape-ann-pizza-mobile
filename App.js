26c97d19e333412873e15832c1e54eb65eed5edf
Emir Šahović <emir.sahovic00@gmail.com>
2022-09-30 10:08:08 +0200

    Redux provider

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import "react-native-gesture-handler";
 import { StatusBar } from "react-native";
 import MyDrawer from "./navigator/MyDrawer";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 export default function App() {
   return (
-    <>
+    <Provider store={store}>
       <StatusBar barStyle={"light-content"} />
       <MyDrawer />
-    </>
+    </Provider>
   );
 }
